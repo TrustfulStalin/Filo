12e26da8907fd44250e4779129f8c48caefdcdd3
Tevon Talbert <TevonTalbert@hotmail.com>
2025-01-28 20:14:50 -0600

    added net end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ import Projects from "./Pages/projects";
 
 function App() {
   // URL should have YOUR HEROKU URL for your backend, make sure you include the trailing slash
-  const URL = "https://portback-816b3f6d02d0.herokuapp.com/";
+  const URL = "https://filoback.netlify.app/index.html";
 
   return (
     <div className="App">
